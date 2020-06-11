345ed39da42f9cf91b509632e3039d28a7a0cce2
Gustavo Santos <gustavofsantos@outlook.com>
2020-06-11 20:30:48 -0300

    Created the mod file

---
+++ mod.ts
@@ -0,0 +1,4 @@
+export {default as cycle} from './cycle.ts';
+export {default as iterate} from './iterate.ts';
+export {default as repeat} from './repeat.ts';
+export {default as take} from './take.ts';
